5ff806dcb2449906aa28f92bf025442958a240df
paszczureKk <dszynszecki@wp.pl>
2018-11-07 11:18:24 +0100

    Auto stash before cherrypick of "Update .gitignore"
    Handlers v.1.0

---
+++ Assets/Scripts/TimeHandler.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class TimeHandler : MonoBehaviour {
+    
+    private float timeSpeed;
+
+    private void Start()
+    {
+        timeSpeed = GameHandler.Instance.TimeSpeed;
+    }
+
+    private void Update()
+    {
+        
+    }
+}
