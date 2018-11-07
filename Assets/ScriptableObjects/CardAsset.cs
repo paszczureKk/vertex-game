b61a79d395a62032c951b395bded489c9d2a3539
paszczureKk <dszynszecki@wp.pl>
2018-11-07 12:17:11 +0100

    Card

---
+++ Assets/ScriptableObjects/CardAsset.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "Card", menuName = "Assets/Card")]
+public class CardAsset : ScriptableObject
+{
+    [TextArea(2,3)]
+    public string opis;
+
+    [Header("Properties")]
+    public int ogień;
+    
+
+}
