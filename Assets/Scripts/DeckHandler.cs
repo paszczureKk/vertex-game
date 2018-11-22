1e72b6a77f24b6f0f97d240ff9dadcc215e05ba6
paszczureKk <dszynszecki@wp.pl>
2018-11-22 15:49:10 +0100

    PROJECT MERGE v.17
    Połączenie gałęzi yeee! + bede-grol-wgre w najnowszej oficjalnej wersji unity

--- Assets/Scripts/DeckHandler.cs
+++ Assets/Scripts/DeckHandler.cs
@@ -5,15 +5,44 @@ public class DeckHandler : MonoBehaviour
 {
     private static DeckHandler instance;
 
+    #region EDITOR_VARS
+
+    //wzorzec karty
     [SerializeField]
     private GameObject cardPrefab;
+    //miejsce inicjalizacji kart
     [SerializeField]
     private Transform spawnPoint;
+    //panel reki
+    [SerializeField]
+    private RectTransform handTransform;
+    //pojemnosc reki
     [SerializeField]
     private int handCap;
 
+    #endregion
+
+    #region DATA_CONTAINERS
+
+    //lista wszystkich kart
     private List<CardAsset> cardData = new List<CardAsset>();
+    //lista kart na rece
     private List<GameObject> cards = new List<GameObject>();
+
+    #endregion
+
+    #region PRIVATE_VARS
+
+    //liczba kart na rece
+    private int cardsCounter;
+    //szerokosc pojedynczej karty
+    private float cardWidth;
+    //pozycja pierwszej karty w wypadku gdy nachodza na siebie (inaczej to spawnPoint)
+    private float firstCardPointer;
+
+    #endregion
+
+    #region PROPERTIES
 
     public int HandCap
     {
@@ -27,6 +56,23 @@ public class DeckHandler : MonoBehaviour
                 handCap = 3;
             else
                 handCap = value;
+        }
+    }
+
+    public int CardsCounter
+    {
+        get
+        {
+            return cardsCounter;
+        }
+        set
+        {
+            if (value < 0)
+                cardsCounter = 0;
+            else
+                cardsCounter = value;
+
+            Settle();
         }
     }
 
@@ -46,6 +92,8 @@ public class DeckHandler : MonoBehaviour
         }
     }
 
+    #endregion
+
     private void Awake()
     {
         if (instance != null)
@@ -53,12 +101,55 @@ public class DeckHandler : MonoBehaviour
         else
             instance = this;
 
+        #region CARD_INIT
         cardData = new List<CardAsset>(Resources.LoadAll<CardAsset>("Cards"));
 
         for (int i = 0; i < handCap; i++)
         {
             GameObject card = Instantiate<GameObject>(cardPrefab, spawnPoint.transform.position, Quaternion.identity, spawnPoint);
             cards.Add(card);
+        }
+        #endregion
+
+        //ustawienie parametrów funkcji Settle()
+        cardWidth = cards[0].GetComponent<RectTransform>().rect.width;
+
+        //ustawienie startowej liczby kart na ręce
+        CardsCounter += handCap;
+    }
+
+    //ustawia karty na swoich miejscach - wywolywana w momencie zmiany liczby posiadanych kart lub posortowania ich
+    private void Settle()
+    {
+        if (CardsCounter * cardWidth + (CardsCounter - 1) * 15.0f > handTransform.rect.width)
+        {
+            firstCardPointer = handTransform.rect.width / 2.0f - cardWidth / 2.0f - 15.0f;
+            float offset = (handTransform.rect.width - cardWidth - 30.0f) / (CardsCounter - 1);
+            for (int i = 0; i < CardsCounter; i++)
+            {
+                cards[i].transform.localPosition -= new Vector3(firstCardPointer, 0.0f, 0.0f) - new Vector3(i * offset, 0.0f, 0.0f);
+            }
+        }
+        else
+        {
+            if (CardsCounter % 2 == 0)
+            {
+                for (int i = 0; i < CardsCounter; i++)
+                {
+                    firstCardPointer = (cardWidth + 15.0f) * (CardsCounter / 2 - 0.5f);
+                    float offset = cardWidth + 15.0f;
+                    cards[i].transform.localPosition -= new Vector3(firstCardPointer, 0.0f, 0.0f) - new Vector3(i * offset, 0.0f, 0.0f);
+                }
+            }
+            else
+            {
+                for (int i = 0; i < CardsCounter; i++)
+                {
+                    firstCardPointer = (cardWidth + 15.0f) * (CardsCounter / 2);
+                    float offset = cardWidth + 15.0f;
+                    cards[i].transform.localPosition -= new Vector3(firstCardPointer, 0.0f, 0.0f) - new Vector3(i * offset, 0.0f, 0.0f);
+                }
+            }
         }
     }
 }
