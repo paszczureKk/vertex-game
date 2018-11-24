a5e4d4332faea0e4d112f4d8ffe0229f24e8eab3
paszczureKk <dszynszecki@wp.pl>
2018-11-24 17:55:23 +0100

    Custom Inspector v.1.0

---
+++ Assets/Editor/CardAssetsEditor.cs
@@ -0,0 +1,26 @@
+﻿using UnityEditor;
+
+[CustomEditor(typeof(CardAsset), true)]
+public class CardAssetsEditor : BaseAssetsEditor
+{
+
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+
+        var objectCardAsset = (CardAsset)target;
+
+        EditorGUILayout.Space();
+        EditorGUILayout.Space();
+        EditorGUILayout.Space();
+        
+        EditorGUILayout.LabelField("Karma", EditorStyles.boldLabel);
+
+        EditorGUILayout.Space();
+
+        EditorGUILayout.BeginHorizontal();
+            EditorGUILayout.LabelField("Karma:");
+            objectCardAsset.karma = EditorGUILayout.IntField(objectCardAsset.karma);
+        EditorGUILayout.EndHorizontal();
+    }
+}
